183d36413f4e345735c58ebe0862e75a6a7c228a
Oskar Enmalm <oskarenmalm@gmail.com>
2018-10-31 23:18:58 +0100

    chunky solution

--- 4.js
+++ 4.js
@@ -12,13 +12,42 @@
 var test1 = [3, 4, -1, 1];
 var test2 = [1, 2, 0];
 var test3 = [-4, 3, 5, 3, 6];
+var test4 = [];
 
-fuction findLowest (arr) {
+console.log(findLowest(test1));
+console.log(findLowest(test2));
+console.log(findLowest(test3));
+console.log(findLowest(test4));
+
+function findLowest (arr) {
+    
     var lowest;
-    if(containsPositive(arr)) {
-        //Implementation
-    } else {
+
+    var positiveArray = arr.filter(ints => {
+        return ints > 0;
+    });;
+
+    if (!Array.isArray(positiveArray)) {
         console.log("There was no positive number in the integer ");
         lowest = 1;
+    } else {
+        lowest = arrayChecker(positiveArray);
+    }
+    return lowest;
+
+    function arrayChecker (postiveSet) {
+        var firstGap;
+        var found = false;
+        var starter = 1;
+
+        while(!found) {
+            if (postiveSet.includes(starter)) {
+                starter ++;
+            } else {
+                firstGap = starter;
+                found = true;
+            }
+        }
+        return firstGap;
     }
 }
